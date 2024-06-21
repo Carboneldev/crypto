4b9222ded2ff4c68555aebaa1e031dfcb0c2fc8d
Carboneldev <carbonelnata@gmail.com>
2024-06-21 22:07:14 +0200

    adaptive style

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,57 +1,28 @@
 <template>
-    <div className="blockInp">
-    <input min="0" type="number" @input="changeAmount($event.target.value)">
-    <button @click="convert()">convert</button>
-</div>
+    <div class="blockInp">
+      <input min="0" type="number" @input="changeAmount($event.target.value)" />
+      <button @click="convert()">convert</button>
+    </div>
   </template>
   
   <script>
-  
-  
-  export default{
-   props:{
-    changeAmount:{   
+  export default {
+    props: {
+      changeAmount: {
         type: Function,
         required: true
-    },
-    convert:{   
+      },
+      convert: {
         type: Function,
         required: true
+      }
     }
-   }
   }
   </script>
   
   <style scoped>
-.blockInp{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-button{
-    position: relative;
-    top: -20px;
-    padding: 15px 20px;
-    color: #fff;
-    text-transform: uppercase;
-    cursor: pointer;
-    background: #1a032d;
-    border: 0;
-    border-radius: 3px;
-    text-align: center;
-}
-
-   input {
-    outline: none;
-    width: 500px;
-    position: relative;
-    top: -50px;
-    font-size: 1.2em;
-    border-radius: 3px;
-    border: 0;
-    padding: 10px 15px;
-    background: #fafafa;
-    color: #333;
-   }
+  
+  
+  
   </style>
+  
--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,57 +1,54 @@
 <template>
-<ul>
-    <li @click="selectItem('BTC')" :className="current == 'BTC' ? 'active' : ''" >Bitcoin</li>
-    <li @click="selectItem('ETH')" :className="current == 'ETH' ? 'active' : ''">ETH</li>
-    <li @click="selectItem('USDT')" :className="current== 'USDT' ? 'active' : ''">USDT</li>
-</ul>
-
-
-</template>
-
-<script>
-
-export default{
-    props:{
-        setCrypto:{
-            type: Function,
-            required: true
-        }
+    <ul>
+      <li @click="selectItem('BTC')" :class="{ active: current === 'BTC' }">Bitcoin</li>
+      <li @click="selectItem('ETH')" :class="{ active: current === 'ETH' }">ETH</li>
+      <li @click="selectItem('USDT')" :class="{ active: current === 'USDT' }">USDT</li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      setCrypto: {
+        type: Function,
+        required: true
+      }
     },
-    data(){
-        return{
-            current: ''
-        }
+    data() {
+      return {
+        current: ''
+      }
     },
-     methods:{
-        selectItem(val){
-            this.setCrypto(val);
-            this.current = val;
-        }
-     }
-}
-
-</script>
-
-<style scoped>
-ul{
+    methods: {
+      selectItem(val) {
+        this.setCrypto(val);
+        this.current = val;
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  ul {
     list-style: none;
     width: 200px;
     padding: 0;
     border-radius: 3px;
     border: 3px solid #000;
-}
-
-li{
+  }
+  
+  li {
     display: block;
     background: #1A032D;
     color: #fff;
     padding: 20px 0;
-}
-
-li:hover, li.active{
+  }
+  
+  li:hover, li.active {
     background: #24043e;
     cursor: pointer;
-}
-
-
-</style>
+  }
+  
+  
+  </style>
+  
